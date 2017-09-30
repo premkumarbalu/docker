9987376eaacae4753b6eae439d260204c5d43afc
Prem Kumar <premkumar.balu@htcindia.com>
2017-09-30 21:42:10 +0530

    Submit Collection Performance fix - updated the branch

--- scsb-circ/shellBuild.sh
+++ scsb-circ/shellBuild.sh
@@ -13,7 +13,7 @@ cd $PROJ_DIR
 git clone https://github.com/premkumarbalu/scsb-circ.git
 # echo $PROJ_DIR_TAG
 cd $PROJ_DIR_TAG
-git checkout SC_Performance
+git checkout SC_Perf-1
 if [ ! -z $1 ] ;then
     git checkout tags/$1
 fi
